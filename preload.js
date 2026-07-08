ca81b6ff4e390f54795634bf6ecdad14a678530a
flight24 <13367733900@qq.com>
2026-07-08 15:43:53 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAutoStart: () => ipcRenderer.invoke('get-auto-start'),
-  setAutoStart: (enabled) => ipcRenderer.invoke('set-auto-start', enabled)
-});
